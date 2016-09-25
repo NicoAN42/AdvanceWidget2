ce5320e3772714408d1a877a2ccc6f833dd8977d
USER-ACER-PC <nicothecruzguardian@gmail.com>
2016-09-25 20:43:23 +0700

    Initial Commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2019/advancedwidget2/MainActivity.java
@@ -0,0 +1,24 @@
+package id.sch.smktelkom_mlg.tugas01.xirpl2019.advancedwidget2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindColor;
+import butterknife.BindView;
+import butterknife.BindViews;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+}
